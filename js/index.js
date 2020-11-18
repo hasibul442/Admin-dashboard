88c4c277cb60786a4695948cf9ccec823bebb412
Mohammad Hasibul Hasan <hasibulhasan442@gmail.com>
2020-11-18 20:56:20 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.nav_btn').click(function(){
+      $('.mobile_nav_items').toggleClass('active');
+    });
+  });
+
+  $(document).ready(function () {
+    $('#dtBasicExample').DataTable();
+    $('.dataTables_length').addClass('bs-select');
+    });
+
+
+
